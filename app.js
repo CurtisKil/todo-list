f1a9f9880da1bf1f4f654852052785f9092f8eb6
Curtis <lucidck17@gmail.com>
2021-09-21 19:10:06 -0700

    UI With Materialize

---
+++ app.js
@@ -0,0 +1,6 @@
+// Define UI Variables
+const form = document.getElementById("todo-form");
+const todoList = document.querySelector(".todo-list");
+const clearBtn = document.querySelector(".clear-todos");
+const filter = document.getElementById("filter");
+const todoInput = document.getElementById("todo");
